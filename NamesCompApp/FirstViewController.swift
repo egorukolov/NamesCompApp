75386cbbca3ad6b31a40f2ce4b5b3c6ec9d4e929
Egor Ukolov <ukolovegor@yandex.ru>
2023-08-10 23:37:05 +0700

    Добавление AlertController при вводе не полном или заполнении textField's

--- NamesCompApp/FirstViewController.swift
+++ NamesCompApp/FirstViewController.swift
@@ -12,11 +12,6 @@ class FirstViewController: UIViewController {
     @IBOutlet var yourNameTF: UITextField!
     @IBOutlet var partnersNameTF: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destinationVC = segue.destination as? ResultViewController else { return }
         destinationVC.firstName = yourNameTF.text
@@ -24,10 +19,34 @@ class FirstViewController: UIViewController {
     }
 
     @IBAction func showResultButton() {
+        guard let firstName = yourNameTF.text,
+                let secondName = partnersNameTF.text else { return }
+        if firstName.isEmpty || secondName.isEmpty {
+            showAlert(title: "Names are missing", message: "Please enter both names😥")
+            return
+        }
+        
+        
         performSegue(withIdentifier: "go2Result", sender: nil)
+        
     }
     
+    @IBAction func unwindSegueToFirstVC(segue: UIStoryboardSegue) {
+        yourNameTF.text = ""
+        partnersNameTF.text = ""
+    }
 
+}
+
+extension FirstViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAlert = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAlert)
+        present(alert, animated: true)
+    }
 }
 
 extension FirstViewController: UITextFieldDelegate{
